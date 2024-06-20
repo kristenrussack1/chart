ce0b7f604f2ef47c4a2d35fd541bbc8fefb337cc
kristenrussack1 <kristenrussack@gmail.com>
2024-06-20 09:33:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -134,8 +134,36 @@
 
 
 
+// const getDataFromBackend = async () => {
+//     const response = await fetch("http://localhost:8008/users");
+//     const data = await response.json();
+//     return data;
+//   };
+  
+//   const addData = async () => {
+//     const data = await getDataFromBackend();
+//     const container = document.getElementById("container");
+  
+//     data.forEach((value) => {
+//       const div = document.createElement("div");
+//       div.classList.add("userContainer");
+//       div.innerHTML = `
+//         <h3>${value.projectName}</h3>
+//         <p><strong>Start Date:</strong> ${value.startDate}</p>
+//         <p><strong>End Date:</strong> ${value.endDate || "N/A"}</p>
+//          <p> ${value.parentItem || "N/A"}</p>
+//         <p> ${value.subItemNames || "N/A"}</p>
+//         <p> ${value.rollupProgress|| "0"}</p>
+//       `;
+//       container.appendChild(div);
+//     });
+//   };
+  
+//   addData();
+
+
 const getDataFromBackend = async () => {
-    const response = await fetch("http://localhost:8008/users");
+    const response = await fetch("http://localhost:8009/users");
     const data = await response.json();
     return data;
   };
